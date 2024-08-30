7b2db2b08028537781f7385d80fb47babe366b1d
Reimend <rayndolo824@gmail.com>
2024-08-30 15:52:21 +0300

    answers to assignment 2

---
+++ list.py
@@ -0,0 +1,47 @@
+
+#Creating an empty list
+
+# my_list = []
+
+#appending a list
+
+# my_list.append(10)
+# my_list.append(20)
+# my_list.append(30)
+# my_list.append(40)
+
+# print (my_list)
+
+#inserting a value to a list at a specific position,2
+
+# my_list = [10,20,30,40]
+# my_list.insert(2, "15")
+
+# print(my_list)
+
+#extending a list by inserting values at the end of the list
+
+# my_list = [10,20,15,30,40]
+# list = [50,60,70]
+# my_list.extend(list)
+
+# print(my_list)
+
+#removing an element from a list
+
+# my_list = [10,20,15,30,40,50,60,70]
+# my_list.remove(70)
+ 
+# print(my_list)
+
+#sorting a list in ascending order
+
+# my_list = [10,20,15,30,40,50,60]
+# my_list.sort()
+
+# print(my_list)
+
+my_list = [10,15,20,30,40,50,60]
+x = my_list.index(30)
+
+print(x)
